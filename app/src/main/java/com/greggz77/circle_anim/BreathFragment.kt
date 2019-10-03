46eceb7ebcae6c4968a88238892fd09240dcc15a
gregz <gregor@zafran.si>
2019-10-03 11:33:00 +0200

    added directions

--- app/src/main/java/com/greggz77/circle_anim/BreathFragment.kt
+++ app/src/main/java/com/greggz77/circle_anim/BreathFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.ALPHA
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_breath.*
@@ -28,14 +29,15 @@ class BreathFragment : Fragment() {
         val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y,1f, 2f)
         val scaleXS = PropertyValuesHolder.ofFloat(View.SCALE_X,2f, 1f)
         val scaleYS = PropertyValuesHolder.ofFloat(View.SCALE_Y,2f, 1f)
-
         val scaleTextUp = ObjectAnimator.ofPropertyValuesHolder(textView, scaleX, scaleY)
-            /*.apply {
-            duration = 3500
-        }*/
         val scaleTextDown = ObjectAnimator.ofPropertyValuesHolder(textView, scaleXS, scaleYS)
-        val animatorSet = AnimatorSet()
+        //Alpha animation for directions
+        val alphaTextIn = PropertyValuesHolder.ofFloat(ALPHA, 1f, 0f)
+        val alphaTextOut = PropertyValuesHolder.ofFloat(ALPHA, 0f, 1f)
+        val alphaIn = ObjectAnimator.ofPropertyValuesHolder(breatheInText, alphaTextIn)
+        val alphaOut = ObjectAnimator.ofPropertyValuesHolder(breatheOutText, alphaTextOut)
 
+        val animatorSet = AnimatorSet()
         //Repeating animation indefinitely
         animatorSet.addListener(object : AnimatorListenerAdapter() {
 
@@ -44,11 +46,13 @@ class BreathFragment : Fragment() {
             override fun onAnimationStart(animation: Animator) {
                 mCanceled = false
                 //Timer countdown to stop anim. after 5 min. - set timer duration to 5' - current duration just for testing
-                object : CountDownTimer(10000, 1000) {
+                object : CountDownTimer(20000, 1000) {
 
                     override fun onTick(millisUntilFinished: Long) {
                     }
                     override fun onFinish() {
+                        breatheInText.text = ""
+                        breatheOutText.text = ""
                         directionsTextView.text = "Exercise done..."
                         mCanceled = true
                     }
@@ -63,7 +67,11 @@ class BreathFragment : Fragment() {
                 }
             }
         })
-        animatorSet.playSequentially(scaleTextUp, scaleTextDown)
+        //animatorSet.playSequentially(scaleTextUp, scaleTextDown)
+        animatorSet.play(scaleTextUp)
+            .with(alphaIn)
+            .before(scaleTextDown)
+                .with(alphaOut)
         animatorSet.duration = 3500
         animatorSet.start()
     }
